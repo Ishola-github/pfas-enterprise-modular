af0c97a6438ca3e0c0ffc1741b6ce43eeb914e06
Ishola-github <121737626+Ishola-github@users.noreply.github.com>
2026-05-16 19:31:54 -0700

    Delete data/external/data/external/nist_pfas directory

--- data/external/data/external/nist_pfas/suspectlist/fn/get_suspectlist.R
+++
@@ -1,7 +0,0 @@
-get_suspectlist <- function(destfile = paste("PFAS Suspect List", Sys.Date(), ".xlsx", sep = "")) {
-  url <- "https://data.nist.gov/od/ds/mds2-2387/PFAS%20Suspect%20List.xlsx"
-  download.file(url, destfile, mode = "wb")
-  if (file.exists(destfile)) {
-    print(paste("The PFAS Suspect List has been successfully downloaded to", destfile))
-  }
-}
